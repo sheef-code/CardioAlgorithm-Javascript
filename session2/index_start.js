66bee15a38a28e9ddd87a9dee8cfd227f752cb86
Shifath Salam <sheef@Shifaths-MacBook-Pro.local>
2019-02-17 08:49:22 -0800

    updated session2 function

--- session2/index_start.js
+++ session2/index_start.js
@@ -1,9 +1,22 @@
 // CHALLENGE 1: LONGEST WORD
 // Return the longest word of a string
-// ex. longestWord('Hi there, my name is Brad') === 'there,'
+// ex. longestWord('Hi there, my name is Brad') === 'there'
 
 function longestWord(sen) {
-  return sen.split(" ").map;
+  // Create filtered array
+  const word = sen.toLowerCase().match(/[a-z0-9]+/g);
+  // Sort by length
+  const longestWord = word.sort((a, b) => b.length - a.length);
+  // If multiple words, put into array
+  const longArr = longestWord.filter(
+    word => word.length === longestWord[0].length
+  );
+  // Check if more than one array val
+  if (longArr.length === 1) {
+    return longArr[0];
+  } else {
+    return longArr;
+  }
 }
 
 // CHALLENGE 2: ARRAY CHUNKING
